3759b9b0f55085b474f4c087b77bc6b3d85dfd7b
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-02-10 01:32:52 +0300

    Added JUnit

---
+++ chapter_001/src/main/java/ru/osetsky/Calculate.java
@@ -0,0 +1,17 @@
+package ru.osetsky;
+
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author osetsky
+ *@since 05.07.2017
+*/
+
+public class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg.
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
