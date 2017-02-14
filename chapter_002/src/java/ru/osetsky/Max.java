5797908a895d29e04f6f0501ab3bf3efc01d8abe
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-02-14 23:26:32 +0300

    Add Max

---
+++ chapter_002/src/java/ru/osetsky/Max.java
@@ -0,0 +1,13 @@
+package ru.osetsky;
+
+public class Max {
+	public static void main(String[] args) {
+		double first = 1, second =2;
+		if (first >=second) {
+			return first;
+		} else {
+			return second;
+		}
+		
+	}
+}
