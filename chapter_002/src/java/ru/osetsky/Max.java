2011ba36be97fc87844faee90ba72a3fd9544ce0
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-02-17 02:10:09 +0300

    Add exercize exercise 3.3 Max

--- chapter_002/src/java/ru/osetsky/Max.java
+++ chapter_002/src/java/ru/osetsky/Max.java
@@ -5,4 +5,15 @@ public class Max {
 		int result = first >= second ? first : second;
 		return result;
 	}
+	
+	public int max (int first, int second, int third) {
+		
+		int resultmax1 = first >= second ? first : second;
+		int resultmax2 = first >= third ? first : third;
+		int resultmax3 = second >= third ? second : third;
+		int resultmax4 = resultmax1 >= resultmax2 ? resultmax1 : resultmax2;
+		int resultmax5 = resultmax4 >= resultmax3 ? resultmax4 : resultmax3;
+		return resultmax5;
+		
+	}
 }
