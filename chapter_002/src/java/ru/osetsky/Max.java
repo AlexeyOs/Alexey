af9e176167c14f8db182a361615cac9d1e91ae33
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-02-19 22:37:12 +0300

    Add change 3

--- chapter_002/src/java/ru/osetsky/Max.java
+++ chapter_002/src/java/ru/osetsky/Max.java
@@ -2,18 +2,10 @@ package ru.osetsky;
 
 public class Max {
 	public int max (int first,int second) {
-		int result = first >= second ? first : second;
-		return result;
+		return first >= second ? first : second;
 	}
 	
-	public int max (int first, int second, int third) {
-		
-		int resultmax1 = first >= second ? first : second;
-		int resultmax2 = first >= third ? first : third;
-		int resultmax3 = second >= third ? second : third;
-		int resultmax4 = resultmax1 >= resultmax2 ? resultmax1 : resultmax2;
-		int resultmax5 = resultmax4 >= resultmax3 ? resultmax4 : resultmax3;
-		return resultmax5;
-		
+	public int max (int first, int second, int third) {	
+		return max(first,max(second,third));	
 	}
 }
