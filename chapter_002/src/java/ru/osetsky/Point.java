61be8faabf7c6d215fdd5a429e691924ff7d6975
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-02-20 20:58:50 +0300

    Add java Point

--- chapter_002/src/java/ru/osetsky/Point.java
+++ chapter_002/src/java/ru/osetsky/Point.java
@@ -1,8 +1,11 @@
 package ru.osetsky;
 
+import java.io.File;
+
 public class Point {
 	public double x;
 	public double y;
+	public boolean bool=true; 
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
@@ -10,8 +13,7 @@ public class Point {
 
 	public double distanceTo(Point point) {
 	//calculate distance between two points 
-	boolean exists("C:\projects\Alexey\chapter_002\src\java\ru\osetsky\Triangle.java");
-	if (exists("C:\projects\Alexey\chapter_002\src\java\ru\osetsky\Triangle.java")	{
+	if (bool == Triangle.exists())	{
 		double dx = this.x-x;
 		double dy = this.y-y;
 		return Math.sqrt(dx*dx+dy*dy);
