22c08e5dcc49eda993835fa544c4abe7abe2932a
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-03-01 19:03:20 +0300

    Add change Square

--- chapter_002/src/main/java/ru/osetsky/Square.java
+++ chapter_002/src/main/java/ru/osetsky/Square.java
@@ -4,8 +4,8 @@ public class Square {
 	public int[][] povorot(int[][] a){
 		int m;
 		int n=2;
-		for (int i; i<=n%2;i++){ 
-			for (int k; k<=(n-i);k++){
+		for (int i=0; i<=n%2;i++){ 
+			for (int k=0; k<=(n-i);k++){
 			m=a[i][k];
 			a[i][k]=a[n-k+1][i];
 			a[n-k+1][i]=a[n-i-1][n-k+1];
