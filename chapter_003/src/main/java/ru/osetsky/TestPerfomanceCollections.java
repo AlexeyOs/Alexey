36632494bc2c076d61a95acbdfd597577107e080
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-07-08 23:15:57 +0300

    Тестирование производительности коллекций [#10022]

---
+++ chapter_003/src/main/java/ru/osetsky/TestPerfomanceCollections.java
@@ -0,0 +1,29 @@
+package ru.osetsky;
+
+import java.util.Collection;
+
+/**
+ * Created by koldy on 08.07.2017.
+ */
+public class TestPerfomanceCollections {
+    /**
+     * Method add is public.
+     * @param collection is String
+     * @param amount type integer.
+     * @return value.
+     */
+    public long add(Collection<String> collection, int amount) {
+        long result = 1;
+        return result;
+    }
+    /**
+     * Method delete is public.
+     * @param collection is String
+     * @param amount is array type integer.
+     * @return value.
+     */
+    public long delete(Collection<String> collection, int amount) {
+        long result = 1;
+        return result;
+    }
+}
