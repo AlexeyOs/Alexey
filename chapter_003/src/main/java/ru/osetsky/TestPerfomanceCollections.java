76f667d35b097f6a72b86fcf86a7978c970781ff
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-07-15 13:29:44 +0300

    Тестирование производительности коллекций2 [#10022]

--- chapter_003/src/main/java/ru/osetsky/TestPerfomanceCollections.java
+++ chapter_003/src/main/java/ru/osetsky/TestPerfomanceCollections.java
@@ -1,6 +1,7 @@
 package ru.osetsky;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by koldy on 08.07.2017.
@@ -13,7 +14,11 @@ public class TestPerfomanceCollections {
      * @return value.
      */
     public long add(Collection<String> collection, int amount) {
-        long result = 1;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < amount; i++) {
+            collection.add("1" + i);
+        }
+        long result = System.currentTimeMillis() - start;
         return result;
     }
     /**
@@ -23,7 +28,13 @@ public class TestPerfomanceCollections {
      * @return value.
      */
     public long delete(Collection<String> collection, int amount) {
-        long result = 1;
+        long start = System.currentTimeMillis();
+        Iterator iter = collection.iterator();
+        while (iter.hasNext()) {
+            iter.next();
+            iter.remove();
+        }
+        long result = System.currentTimeMillis() - start;
         return result;
     }
 }
---
+++ chapter_003/src/test/java/ru/osetsky/TestTestPerfomanceCollections.java
@@ -0,0 +1,51 @@
+package ru.osetsky;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+ /**
+   * Created by koldy on 14.07.2017.
+   */
+public class TestTestPerfomanceCollections {
+    /**
+     * Method is testSpeedAddAndDeleteList .
+     */
+    @Test
+    public void testSpeedAddAndDeleteList() {
+         TestPerfomanceCollections test = new TestPerfomanceCollections();
+         List<String> list = new LinkedList<String>();
+         System.out.println(test.add(list, 1000000));
+         System.out.println(test.delete(list, 1000000));
+     }
+    /**
+      * Method is testSpeedAddAndDeleteArr.
+      */
+    @Test
+    public void testSpeedAddAndDeleteArr() {
+          TestPerfomanceCollections test = new TestPerfomanceCollections();
+          List<String> arr = new ArrayList<String>();
+          System.out.println(test.add(arr, 1000000));
+          System.out.println(test.delete(arr, 1000000));
+      }
+    /**
+      * Method is testSpeedAddAndDeleteTree.
+      */
+    @Test
+    public void testSpeedAddAndDeleteTree() {
+        TestPerfomanceCollections test = new TestPerfomanceCollections();
+        TreeSet<String> tree = new TreeSet<String>();
+        System.out.println(test.add(tree, 1000000));
+        System.out.println(test.delete(tree, 1000000));
+    }
+     /**
+      * LinkedList add:238     delete:39
+      * ArrayList  add:954     delete:116
+      * TreeSet    add:112     delete:133555
+      * Лучшее время показал массив LinkedList, так как
+      * там было удаление с начала списка, похуже результат показал ArrayList
+      * И дольше всего по сумме времени добавление и удаления показал себя TreeSet,
+      * очень долго выполялось удаление, так как много времени уходило на перемещение по дереву(массив TreeSet)
+      */
+ }
