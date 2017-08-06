469867565dca109e6765cdb4f01bdbf14f2ebd3d
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-08-06 18:30:04 +0300

    Конвертация двумерного массива в ArrayList и наоборот2 [#10035]

--- chapter_003/src/main/java/ru/osetsky/ConvertList.java
+++ chapter_003/src/main/java/ru/osetsky/ConvertList.java
@@ -1,14 +1,18 @@
 package ru.osetsky;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Created by koldy on 09.07.2017.
  */
 public class ConvertList {
-
+    /**
+     * Method List is transformation Array to ArrayList.
+     * @param array is int[][].
+     * @return result.
+     */
     public List<Integer> toList(int[][] array) {
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < array.length; i++) {
@@ -18,8 +22,32 @@ public class ConvertList {
         }
         return result;
     }
-    public int[][] toArray (List<Integer> list, int rows) {
-        int arr[][]= new int[2][3];
-        return arr;
+
+    /**
+     * Method List is transformation List to Array.
+     * @param list is tyoe List<integer>
+     * @param rows is type integer.
+     * @return result.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        Iterator<Integer> iter = list.iterator();
+        int stolbec;
+        if (list.size() % rows == 0) {
+            stolbec = list.size() / rows;
+        } else {
+            stolbec = (int) (list.size() / rows) + 1;
+        }
+        int result[][] = new int[rows][stolbec];
+        for (int i = 0; i < stolbec; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (iter.hasNext()) {
+                    result[i][j] = iter.next();
+                } else {
+                    result[i][j] = 0;
+                }
+            }
+        }
+        return result;
     }
+
 }
--- chapter_003/src/test/java/ru/osetsky/TestConvertList.java
+++ chapter_003/src/test/java/ru/osetsky/TestConvertList.java
@@ -24,4 +24,32 @@ public class TestConvertList {
         List<Integer> result = conv.toList(mas);
         assertThat(testData, is(result));
     }
+    /**
+     * Test convert array to arrayList.
+     */
+    @Test
+    public void whenArray1ThenArrayList1() {
+        ConvertList conv = new ConvertList();
+        List<Integer> testData = new ArrayList<Integer>();
+        testData.add(1); testData.add(2); testData.add(3); testData.add(4); testData.add(5); testData.add(6);
+        testData.add(7); testData.add(8);
+        int[][] mas = new int[][] {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+        List<Integer> result = conv.toList(mas);
+        assertThat(testData, is(result));
+    }
+
+    /**
+     * Test convert array to arrayList.
+     */
+    @Test
+    public void whenArrayListThenArray() {
+        ConvertList conv = new ConvertList();
+        int[][] testData = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1); list.add(2); list.add(3); list.add(4); list.add(5); list.add(6);
+        list.add(7);
+        int[][] result = conv.toArray(list, 3);
+        assertThat(testData, is(result));
+    }
+
 }
