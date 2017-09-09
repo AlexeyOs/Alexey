34ecb5d292de5dc7009eed36815a2993a7513829
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-09-09 13:46:43 +0300

    Тестирование производительности коллекций. [#10022] Part2

--- chapter_003/src/test/java/ru/osetsky/testcollections/TestTestPerfomanceCollections.java
+++ chapter_003/src/test/java/ru/osetsky/testcollections/TestTestPerfomanceCollections.java
@@ -16,8 +16,8 @@ public class TestTestPerfomanceCollections {
     public void testSpeedAddAndDeleteList() {
          TestPerfomanceCollections test = new TestPerfomanceCollections();
          List<String> list = new LinkedList<String>();
-         System.out.println(test.add(list, 1000000));
-         System.out.println(test.delete(list, 1000000));
+         System.out.println("LinkedList add = "+test.add(list, 1000000));
+         System.out.println("LinkedList delete = "+test.delete(list, 1000000));
      }
     /**
       * Method is testSpeedAddAndDeleteArr.
@@ -26,8 +26,8 @@ public class TestTestPerfomanceCollections {
     public void testSpeedAddAndDeleteArr() {
           TestPerfomanceCollections test = new TestPerfomanceCollections();
           List<String> arr = new ArrayList<String>();
-          System.out.println(test.add(arr, 1000000));
-          System.out.println(test.delete(arr, 1000000));
+          System.out.println("ArrayList add = "+test.add(arr, 1000000));
+          System.out.println("ArrayList delere = "+test.delete(arr, 1000000));
       }
     /**
       * Method is testSpeedAddAndDeleteTree.
@@ -36,16 +36,20 @@ public class TestTestPerfomanceCollections {
     public void testSpeedAddAndDeleteTree() {
         TestPerfomanceCollections test = new TestPerfomanceCollections();
         TreeSet<String> tree = new TreeSet<String>();
-        System.out.println(test.add(tree, 1000000));
-        System.out.println(test.delete(tree, 1000000));
+        System.out.println("TreeSet add = "+test.add(tree, 1000000));
+        System.out.println("TreeSet delete = "+test.delete(tree, 1000000));
     }
      /**
-      * LinkedList add:238     delete:39
-      * ArrayList  add:954     delete:116
-      * TreeSet    add:112     delete:133555
-      * Лучшее время показал массив LinkedList, так как
-      * там было удаление с начала списка, похуже результат показал ArrayList
-      * И дольше всего по сумме времени добавление и удаления показал себя TreeSet,
-      * очень долго выполялось удаление, так как много времени уходило на перемещение по дереву(массив TreeSet)
+      * LinkedList add:219     delete:47
+      * ArrayList  add:115     delete:115083
+      * TreeSet    add:732     delete:94
+      * 
+      * Лучшее время по добавлению показал ArrayList, но в тоже время ArrayList был худшим по
+      * удалению элементов из массива, так как каждый раз приходится перебирать весь массив, чтобы удалить один элемент
+      * На втором месте по добавление элементов стал LinkedList, и первым по скорости удаления элементов, что логично,
+      * так как в двусвязном списке, чтобы удлаить элемент, нужно задействовать только два соседних элемента.
+      * На третьем месте по скорости добавления стал TreeSet, и на втором по скорости удаления, такая медленная скорость
+      * добавления элементов объясняется необходимостью часто перемещаться с ветки на ветку.
+      *
       */
  }
