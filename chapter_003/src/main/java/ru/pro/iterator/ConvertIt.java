120e6024fcbd95d44fa5c7c9ce2eebc504d74f36
Alexey Osetsky <alexeyosetsky@gmail.com>
2017-09-14 10:38:17 +0300

    Создать convert(Iterator<Iterator>) [#152] Part2

--- chapter_003/src/main/java/ru/pro/iterator/ConvertIt.java
+++ chapter_003/src/main/java/ru/pro/iterator/ConvertIt.java
@@ -1,36 +1,40 @@
 package ru.pro.iterator;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by koldy on 07.09.2017.
  */
-public class ConvertIt {
+class ConvertIt {
     /**
      * Method convert is converting It Iterator<Iterator<Integer>> to Iterator<Integer>.
      * @param it is Iterator<Iterator<Integer>>.
      * @return is Iterator<Integer>.
      */
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-        ArrayList list = new ArrayList();
-        while (it.hasNext()) {
-            Iterator<Integer> iter = it.next();
-            while (iter.hasNext()) {
-                list.add(iter.next());
-            }
-        }
-        return list.iterator();
-    };
+        return new Iterator<Integer>() {
+            private Iterator<Integer> currentIterator = null;
 
-//    @Override
-//    public boolean hasNext() {
-//        return false;
-//    }
-//
-//    @Override
-//    public Object next() {
-//        return null;
-//    }
+            @Override
+            public boolean hasNext() {
+                if (currentIterator == null) {
+                    return false;
+                }
+                if (currentIterator.hasNext()) {
+                    return true;
+                }
+                return currentIterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (currentIterator == null) {
+                    throw new NoSuchElementException();
+                }
+                return currentIterator.next();
+            }
+        };
+    }
 
 }
