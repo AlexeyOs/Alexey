3d0ed687fc734db2f95fe2ceec356a98a41ec5de
Alexey Osetsky <alexeyosetsky@gmail.com>
2018-03-19 23:01:19 +0300

    3. Реализовать собственный механизм блокировок Lock.  [#1100] Addition2

--- chapter_007/src/main/java/ru/osetsky/wait_notify_notifyall/threadpool/Lock.java
+++ chapter_007/src/main/java/ru/osetsky/wait_notify_notifyall/threadpool/Lock.java
@@ -4,23 +4,31 @@ package ru.osetsky.wait_notify_notifyall.threadpool;
  * Created by koldy on 14.03.2018.
  */
 public class Lock{
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Object object = new Object();//создал объект
         Thread thread = new Thread(new ObjectLocker(object));//Привязал объект к потоку
         thread.start();//запустил поток
-        lock(object);//блокирую поток
-        unlock(thread);//заново запускаю
+        lock();//блокирую поток
+        unlock();//заново запускаю
     }
 
-    private static void lock(Object object) {
-        synchronized (object) {
-            object.notify();//блокирую поток
+    private static void lock() throws InterruptedException {
+        if (!isLocked) {
+                isLocked = true;
+                owner = Thread.currentThread();//запихиваю в owner текущий поток
+        } else {
+            Thread.currentThread().wait();
         }
     }
-
-    private static void unlock(Thread thread) {
-        synchronized (thread) {
-            thread.start();//запускаю заново
+    private static boolean isLocked = false;
+    private static Thread owner;
+    private static void unlock() {
+        if (Thread.currentThread() == owner) {
+            synchronized (owner) {
+                isLocked = false;
+                owner.notify();//разбуть поток захваченный owner
+            }
         }
     }
 
