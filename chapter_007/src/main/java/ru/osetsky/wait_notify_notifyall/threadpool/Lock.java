5799df33efee74ff67600b83394b712ef08272fe
Alexey Osetsky <alexeyosetsky@gmail.com>
2018-03-21 00:56:23 +0300

    3. Реализовать собственный механизм блокировок Lock. [ #1100] Addition3

--- chapter_007/src/main/java/ru/osetsky/wait_notify_notifyall/threadpool/Lock.java
+++ chapter_007/src/main/java/ru/osetsky/wait_notify_notifyall/threadpool/Lock.java
@@ -4,31 +4,28 @@ package ru.osetsky.wait_notify_notifyall.threadpool;
  * Created by koldy on 14.03.2018.
  */
 public class Lock{
-
+    private static boolean isLocked = false;
+    private static Thread owner;
     public static void main(String[] args) throws InterruptedException {
         Object object = new Object();//создал объект
         Thread thread = new Thread(new ObjectLocker(object));//Привязал объект к потоку
         thread.start();//запустил поток
-        lock();//блокирую поток
-        unlock();//заново запускаю
+        Lock l = new Lock();
+        l.lock();//блокирую поток
+        l.unlock();//заново запускаю
     }
-
-    private static void lock() throws InterruptedException {
-        if (!isLocked) {
-                isLocked = true;
-                owner = Thread.currentThread();//запихиваю в owner текущий поток
-        } else {
-            Thread.currentThread().wait();
-        }
+    public synchronized void lock() throws InterruptedException{
+        while (isLocked){         // проверяем если текущий объект залочен, то
+            wait();               // ждем уведомления от другого потока, при реактивации будет снова проверен предикат
+        }                         // если isLocked будет true, то снова ожидаем (wait), иначе выходим из цикла while
+        isLocked = true;                 // лочим текущий объект.
+        owner = Thread.currentThread();  // записываем owner'ом текущий поток.
     }
-    private static boolean isLocked = false;
-    private static Thread owner;
-    private static void unlock() {
-        if (Thread.currentThread() == owner) {
-            synchronized (owner) {
-                isLocked = false;
-                owner.notify();//разбуть поток захваченный owner
-            }
+    public synchronized void unlock(){
+        if(Thread.currentThread() == owner){
+            isLocked = false;
+            owner = null;
+            notifyAll();
         }
     }
 
