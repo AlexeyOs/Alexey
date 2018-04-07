e7c0e249621c43a22a18131033b2c5893a959a43
Alexey Osetsky <alexeyosetsky@gmail.com>
2018-04-08 01:10:46 +0300

    Перенос файл сделан для лучшего структурирования проекта

---
+++ chapter_008/src/main/java/ru/osetsky/query/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Данные запросы были сделаны для ознакомления с базой данных PostgreeSQL.
+ * @author Alexey Osetsky (alexeyosetsky@gmail.com)
+ * @version $Id$
+ * @since 08.04.2018
+ */
+package ru.osetsky.query;
