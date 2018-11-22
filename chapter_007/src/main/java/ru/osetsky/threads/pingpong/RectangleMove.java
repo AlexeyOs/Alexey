04db45516ddae75ad5ffc17ae49c64bc8dc1afbc
a.osetckiy <a.osetckiy@extracode.net>
2018-11-22 14:02:08 +0300

    Пинг-понг[#72385] Additional 1

--- chapter_007/src/main/java/ru/osetsky/threads/pingpong/RectangleMove.java
+++ chapter_007/src/main/java/ru/osetsky/threads/pingpong/RectangleMove.java
@@ -10,22 +10,22 @@ public class RectangleMove implements Runnable {
     }
 
     private boolean wallStopByX = false;
-    private boolean wallStopByY = false;
 
     /**
-     * Добавил 4-е if для обработки 4-ёх случаев столкновений со стеной, после столкновения, происходит небольшая задержка.
+     * Добавил 2-е if для обработки 2-ух случаев столкновений со стеной, после столкновения, происходит небольшая задержка.
      */
     @Override
     public void run() {
         while (true) {
-            if (!wallStopByX && !wallStopByY) {
+            if (!wallStopByX) {
+                //обычный шаг вправо
                 this.rect.setX(this.rect.getX() + 1);
-                this.rect.setY(this.rect.getY() + 1);
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                //если справа квадрат уперся, то меняю значение булевской переменной
                 if (this.rect.getX() > 290) {
                     try {
                         Thread.sleep(1000);
@@ -34,82 +34,20 @@ public class RectangleMove implements Runnable {
                         e.printStackTrace();
                     }
                 }
-                if (this.rect.getY() > 290) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByY = true;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else if (wallStopByX && !wallStopByY){
+            }
+            else if (wallStopByX) {
+                //обычный шаг влево
                 this.rect.setX(this.rect.getX() - 1);
-                this.rect.setY(this.rect.getY() + 1);
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if (this.rect.getX() < 1) {
+                //если слева квадрат уперся, то меняю значение булевской переменной
+                if (this.rect.getX() < 0) {
                     try {
                         Thread.sleep(1000);
                         wallStopByX = false;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (this.rect.getY() > 290) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByY = true;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else if (!wallStopByX && wallStopByY) {
-                this.rect.setX(this.rect.getX() + 1);
-                this.rect.setY(this.rect.getY() - 1);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (this.rect.getX() > 290) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByX = true;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (this.rect.getY() < 1) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByY = false;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                this.rect.setX(this.rect.getX() - 1);
-                this.rect.setY(this.rect.getY() - 1);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (this.rect.getX() < 1) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByX = false;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (this.rect.getY() < 1) {
-                    try {
-                        Thread.sleep(1000);
-                        wallStopByY = false;
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
