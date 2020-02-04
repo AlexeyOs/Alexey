85ab830b7f297eb4b13518c060cc5d4c0fd49fa3
a.osetckiy <a.osetckiy@extracode.net>
2020-02-04 16:28:32 +0300

    1.1. Состояние нити.[229175#231334] Версия_2

--- chapter_007/src/main/java/ru/osetsky/concurrent/ThreadState.java
+++ chapter_007/src/main/java/ru/osetsky/concurrent/ThreadState.java
@@ -4,21 +4,19 @@ public class ThreadState {
     /**
      * Нить main должна дождаться завершения двух созданных нитей и вывести на консоль сообщение, что работа завершена.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
-                () -> {}
+                () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getName());
         first.start();
 
         Thread second = new Thread(
-                () -> {}
+                () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(second.getName());
         second.start();
 
         while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
-            System.out.println("Waiting ...");
+            Thread.sleep(1000);
         }
         System.out.println("Work completed");
     }
