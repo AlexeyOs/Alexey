32057a20bde6374bd995b8bcf299b22f0eb95935
a.osetckiy <a.osetckiy@extracode.net>
2020-03-30 12:50:28 +0300

    2.2. Лямбда блок[#249472] Версия_2

--- chapter_004/src/main/java/ru/osetsky/lambda/StringComparison.java
+++ chapter_004/src/main/java/ru/osetsky/lambda/StringComparison.java
@@ -4,15 +4,15 @@ import java.util.Comparator;
 
 public class StringComparison {
 
-    public Comparator compareStringByDefault() {
-        return (Comparator<String>) (left, right) -> {
+    public Comparator<String> compareStringByDefault() {
+        return (left, right) -> {
             System.out.println("compare - left " + left + " : right " + right);
             return left.compareTo(right);
         };
     }
 
-    public Comparator compareStringByDescendingLength(){
-        return (Comparator<String>) (left, right)-> {
+    public Comparator<String> compareStringByDescendingLength(){
+        return (left, right)-> {
             System.out.println("compare - right.length() " + right.length() + " : left.length()" + left.length());
             return right.length() - left.length();
         };
